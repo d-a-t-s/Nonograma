f41fc073eab377b0ca7bfb857e27db2ab1771174
Matfigueroa20 <matfigueroa2023@udec.cl>
2024-10-16 00:36:19 -0300

    TDD.Board - Adding failing test for nxn boards

--- test_board.py
+++ test_board.py
@@ -2,16 +2,19 @@ import pytest
 from createBoard import createBoard
 
 def test_board():
-    board = createBoard()
-    assert len(board) == 5 
-    assert all(len(row) == 5 for row in board)
+    n = 6
+    board = createBoard(n)
+    assert len(board) == n
+    assert all(len(row) == n for row in board)
 
 def test_column_has_true():
-    board = createBoard()
-    for j in range(5):
-        assert any(board[i][j] for i in range(5))
+    n = 5
+    board = createBoard(n)
+    for j in range(n):
+        assert any(board[i][j] for i in range(n))
 
-def test_row_has_true():
+def test_row_has_true(n):
+    n = 6
     board = createBoard()
     for row in board:
         assert any(row)
