85ac272e2e534383a785725300b8648316ae1e39
Matfigueroa20 <matfigueroa2023@udec.cl>
2024-10-16 00:31:08 -0300

    TDD.Board - Adding minimal implementation to pass the 3rd test

--- createBoard.py
+++ createBoard.py
@@ -2,9 +2,15 @@ import random as rn
 
 def createBoard():
     board = [[rn.choice([True, False]) for _ in range(5)] for _ in range(5)]
-    
+
+    #Ensure that each column has at least one True
     for j in range(5):
         if not any(board[i][j] for i in range(5)):
             board[rn.randrange(0, 5)][j] = True
+    
+    #Ensure that each row has at least one True
+    for i in range(5):
+        if not any(board[i][j] for j in range(5)):
+            board[i][rn.randrange(0, 5)] = True
     
     return board
