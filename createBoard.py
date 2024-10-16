42acfa9bac056741d3892f537e732fe58bf69121
Matfigueroa20 <matfigueroa2023@udec.cl>
2024-10-16 01:13:00 -0300

    TDD.Board - Refactoring the function: adding lambda function

--- createBoard.py
+++ createBoard.py
@@ -1,7 +1,7 @@
 import random as rn
 
 def createBoard(num):
-    board = [[rn.choice([True, False]) for _ in range(num)] for _ in range(num)]
+    board = list(map(lambda x: list(map(lambda x: rn.choice([True, False]), range(num))), range(num)))
 
     #Ensure that each column has at least one True
     for j in range(5):
