9feaf4f6f0ee73f6b92d7597669b8749d3fcc7b2
Matfigueroa20 <matfigueroa2023@udec.cl>
2024-10-15 23:59:21 -0300

    TDD.Board - Adding another failing test to check that the board has a true value

--- test_board.py
+++ test_board.py
@@ -5,3 +5,9 @@ def test_board():
     board = createBoard()
     assert len(board) == 5 
     assert all(len(row) == 5 for row in board)
+
+def test_columnas_tienen_true_5x5():
+    """Verifica que cada columna de un tablero 5x5 tenga al menos un True"""
+    tablero = createBoard(5)
+    for j in range(5):
+        assert any(tablero[i][j] for i in range(5))
