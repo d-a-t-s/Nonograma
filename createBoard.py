fbfdd55a7345d71ab44766f335496cd6d7334a7d
Matfigueroa20 <matfigueroa2023@udec.cl>
2024-10-16 00:16:12 -0300

    TDD.Board - Adding minimal implementation to pass the 2nd test

--- createBoard.py
+++ createBoard.py
@@ -1,3 +1,10 @@
+import random as rn
+
 def createBoard():
-    board = [[0 for _ in range(5)] for _ in range(5)]
+    board = [[rn.choice([True, False]) for _ in range(5)] for _ in range(5)]
+    
+    for j in range(5):
+        if not any(board[i][j] for i in range(5)):
+            board[rn.randrange(0, 5)][j] = True
+    
     return board
--- test_board.py
+++ test_board.py
@@ -6,8 +6,7 @@ def test_board():
     assert len(board) == 5 
     assert all(len(row) == 5 for row in board)
 
-def test_columnas_tienen_true_5x5():
-    """Verifica que cada columna de un tablero 5x5 tenga al menos un True"""
-    tablero = createBoard(5)
+def test_column_has_true():
+    board = createBoard()
     for j in range(5):
-        assert any(tablero[i][j] for i in range(5))
+        assert any(board[i][j] for i in range(5))
